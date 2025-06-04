9bf214f5a420a952de4cdf97846ee2ab3c52167d
darwinrobalino12 <dv.robalinob@uea.edu.ec>
2025-06-04 01:02:42 -0500

    tarea seman 2

--- tarea semana 2/clases figuras geometricas.py
+++ tarea semana 2/clases figuras geometricas.py
@@ -59,3 +59,5 @@ if __name__ == "__main__":
     print(f"Área del cuadrado: {mi_cuadrado.calcular_area()}")
     print(f"Perímetro del cuadrado: {mi_cuadrado.calcular_perimetro()}")
     print("--------------------------------\n")
+
+
